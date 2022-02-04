d027b239443ab9d8f0aad083f43471de51f23d94
shlokster <bhosaleshlok@gmail.com>
2022-02-04 16:54:51 +0530

    proof of work

--- main.go
+++ main.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"strconv"
+
+	"github.com/shlokster/Golang-Blockchain/blockchain"
 )
 
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(10))
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("Second Block after Genesis")
+	chain.AddBlock("Third Block after Genesis")
+
+	for _, block := range chain.Blocks {
+
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+	}
 }
